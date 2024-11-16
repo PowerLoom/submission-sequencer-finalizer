8ecfc2bee985e86c32d756b3a03582dd6174a7e8
Chetna Mittal <chetna.mittal125@gmail.com>
2024-11-16 23:26:53 +0530

    feat: add functionality to send updated eligible submission count to relayer every 5 epochs

--- pkgs/batcher/submissionFinalizer.go
+++ pkgs/batcher/submissionFinalizer.go
@@ -123,7 +123,7 @@ func (s *SubmissionDetails) FinalizeBatch() (*ipfs.BatchSubmission, error) {
 	log.Infof("🌳 Merkle tree successfully constructed for batch %d, CID %s, epoch %s in data market %s", s.BatchID, finalizedBatchSubmission.CID, s.EpochID.String(), s.DataMarketAddress)
 
 	// Submit finalized batch submission to the external tx Relayer service with retry mechanism
-	err = s.submitWithRetries(finalizedBatchSubmission)
+	err = s.sendSubmissionBatchToRelayer(finalizedBatchSubmission)
 	if err != nil {
 		log.Errorf("Failed to send batch %d submission with CID %s to the relayer for epoch %s in data market %s: %v", s.BatchID, finalizedBatchSubmission.CID, s.EpochID.String(), s.DataMarketAddress, err)
 		return nil, err
@@ -133,43 +133,6 @@ func (s *SubmissionDetails) FinalizeBatch() (*ipfs.BatchSubmission, error) {
 
 	// Return the finalized batch submission
 	return finalizedBatchSubmission, nil
-}
-
-func (s *SubmissionDetails) submitWithRetries(finalizedBatchSubmission *ipfs.BatchSubmission) error {
-	// Define the operation that will be retried
-	operation := func() error {
-		// Attempt to submit the batch
-		err := clients.SubmitSubmissionBatch(
-			s.DataMarketAddress,
-			finalizedBatchSubmission.CID,
-			s.EpochID,
-			finalizedBatchSubmission.Batch.PIDs,
-			finalizedBatchSubmission.Batch.CIDs,
-			string(finalizedBatchSubmission.FinalizedCIDsRootHash),
-		)
-		if err != nil {
-			log.Errorf("Batch %d submission failed for epoch %s in data market %s: %v. Retrying...", s.BatchID, s.EpochID.String(), s.DataMarketAddress, err)
-			return err // Return error to trigger retry
-		}
-
-		log.Infof("📤 Successfully submitted batch %d with CID %s to relayer for epoch %s in data market %s", s.BatchID, finalizedBatchSubmission.CID, s.EpochID.String(), s.DataMarketAddress)
-		return nil // Successful submission, no need for further retries
-	}
-
-	// Customize the backoff configuration
-	backoffConfig := backoff.NewExponentialBackOff()
-	backoffConfig.InitialInterval = 1 * time.Second // Start with a 1-second delay
-	backoffConfig.Multiplier = 1.5                  // Increase interval by 1.5x after each retry
-	backoffConfig.MaxInterval = 4 * time.Second     // Set max interval between retries
-	backoffConfig.MaxElapsedTime = 10 * time.Second // Retry for a maximum of 10 seconds
-
-	// Limit retries to 3 times within 10 seconds
-	if err := backoff.Retry(operation, backoff.WithMaxRetries(backoffConfig, 3)); err != nil {
-		log.Errorf("Failed to submit batch %d submission with CID %s to relayer for epoch %s in data market %s after retries: %v", s.BatchID, finalizedBatchSubmission.CID, s.EpochID.String(), s.DataMarketAddress, err)
-		return err
-	}
-
-	return nil
 }
 
 func (s *SubmissionDetails) UpdateEligibleSubmissionCounts(batch map[string][]string, projectMostFrequentCID, submissionSnapshotCIDMap map[string]string) error {
@@ -223,11 +186,86 @@ func (s *SubmissionDetails) UpdateEligibleSubmissionCounts(batch map[string][]st
 		key := redis.EligibleSlotSubmissionKey(s.DataMarketAddress, slotID, currentDay.String())
 		updatedCount, err := redis.IncrBy(context.Background(), key, int64(submissionCount))
 		if err != nil {
-			log.Errorf("Failed to update eligible submission count for slotID %s in epoch %s within data market %s: %v", slotID, s.EpochID, dataMarketAddress, err)
+			log.Errorf("Failed to update eligible submission count for slotID %s in batch %d, epoch %s within data market %s: %v", slotID, s.BatchID, s.EpochID, dataMarketAddress, err)
 			return err
 		}
 
-		log.Debugf("✅ Successfully updated eligible submission count for slotID %s in epoch %s within data market %s: %d", slotID, s.EpochID, dataMarketAddress, updatedCount)
+		log.Debugf("✅ Successfully updated eligible submission count for slotID %s in batch %d, epoch %s within data market %s: %d", slotID, s.BatchID, s.EpochID, dataMarketAddress, updatedCount)
+
+		// Send submission count to relayer only if the current epoch is a multiple of 5
+		if s.EpochID.Int64()%5 == 0 {
+			if err := s.sendEligibleSubmissionCountToRelayer(slotID, currentDay.String(), updatedCount); err != nil {
+				log.Errorf("Failed to send eligible submission count to relayer for slotID %s of batch %d, epoch %s within data market %s: %v", slotID, s.BatchID, s.EpochID, s.DataMarketAddress, err)
+				return err
+			}
+		}
+	}
+
+	return nil
+}
+
+func (s *SubmissionDetails) sendEligibleSubmissionCountToRelayer(slotID, currentDay string, submissionCount int64) error {
+	// Define the operation that will be retried
+	operation := func() error {
+		// Attempt to send the eligible submission count
+		err := clients.SendSubmissionCount(s.DataMarketAddress, slotID, currentDay, s.EpochID, int(submissionCount))
+		if err != nil {
+			log.Errorf("Error sending eligible submission count for slotID %s, batch %d, epoch %s in data market %s: %v. Retrying...", slotID, s.BatchID, s.EpochID.String(), s.DataMarketAddress, err)
+			return err // Return error to trigger retry
+		}
+
+		log.Infof("📤 Successfully sent eligible submission count %d for slotID %s, batch %d, epoch %s to relayer in data market %s", submissionCount, slotID, s.BatchID, s.EpochID.String(), s.DataMarketAddress)
+		return nil // Successful submission, no need for further retries
+	}
+
+	// Customize the backoff configuration
+	backoffConfig := backoff.NewExponentialBackOff()
+	backoffConfig.InitialInterval = 1 * time.Second // Start with a 1-second delay
+	backoffConfig.Multiplier = 1.5                  // Increase interval by 1.5x after each retry
+	backoffConfig.MaxInterval = 4 * time.Second     // Set max interval between retries
+	backoffConfig.MaxElapsedTime = 10 * time.Second // Retry for a maximum of 10 seconds
+
+	// Limit retries to a maximum of 3 attempts within 10 seconds
+	if err := backoff.Retry(operation, backoff.WithMaxRetries(backoffConfig, 3)); err != nil {
+		log.Errorf("Failed to send eligible submission count after retries for slotID %s, batch %d, epoch %s in data market %s: %v", slotID, s.BatchID, s.EpochID.String(), s.DataMarketAddress, err)
+		return err
+	}
+
+	return nil
+}
+
+func (s *SubmissionDetails) sendSubmissionBatchToRelayer(finalizedBatchSubmission *ipfs.BatchSubmission) error {
+	// Define the operation that will be retried
+	operation := func() error {
+		// Attempt to submit the batch
+		err := clients.SubmitSubmissionBatch(
+			s.DataMarketAddress,
+			finalizedBatchSubmission.CID,
+			s.EpochID,
+			finalizedBatchSubmission.Batch.PIDs,
+			finalizedBatchSubmission.Batch.CIDs,
+			string(finalizedBatchSubmission.FinalizedCIDsRootHash),
+		)
+		if err != nil {
+			log.Errorf("Batch %d submission failed for epoch %s in data market %s: %v. Retrying...", s.BatchID, s.EpochID.String(), s.DataMarketAddress, err)
+			return err // Return error to trigger retry
+		}
+
+		log.Infof("📤 Successfully submitted batch %d with CID %s to relayer for epoch %s in data market %s", s.BatchID, finalizedBatchSubmission.CID, s.EpochID.String(), s.DataMarketAddress)
+		return nil // Successful submission, no need for further retries
+	}
+
+	// Customize the backoff configuration
+	backoffConfig := backoff.NewExponentialBackOff()
+	backoffConfig.InitialInterval = 1 * time.Second // Start with a 1-second delay
+	backoffConfig.Multiplier = 1.5                  // Increase interval by 1.5x after each retry
+	backoffConfig.MaxInterval = 4 * time.Second     // Set max interval between retries
+	backoffConfig.MaxElapsedTime = 10 * time.Second // Retry for a maximum of 10 seconds
+
+	// Limit retries to 3 times within 10 seconds
+	if err := backoff.Retry(operation, backoff.WithMaxRetries(backoffConfig, 3)); err != nil {
+		log.Errorf("Failed to submit batch %d submission with CID %s to relayer for epoch %s in data market %s after retries: %v", s.BatchID, finalizedBatchSubmission.CID, s.EpochID.String(), s.DataMarketAddress, err)
+		return err
 	}
 
 	return nil
